b6548a3522ae73edfdf145fb271ebd40678390f4
Joseph Britton <85815005+JoeeBritton@users.noreply.github.com>
2023-12-11 01:27:31 -0800

    Track own color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    "tanvirtin/monokai.nvim",
+    "JoeeBritton/nvim-juliana",
     priority = 1000,
     lazy = false,
     config = function()
-        vim.cmd.colorscheme "monokai"
+        vim.cmd.colorscheme "juliana"
     end,
 }
