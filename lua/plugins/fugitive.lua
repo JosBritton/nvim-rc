be41214b81b812e0199118d09376db11a9c7a041
Joseph Britton <85815005+JoeeBritton@users.noreply.github.com>
2023-12-13 23:04:19 -0800

    Fugitive: Add keymap for browsing upstream

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,5 +2,6 @@ return {
     "tpope/vim-fugitive",
     init = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
+        vim.keymap.set("n", "<leader>gb", vim.cmd.GBrowse, { desc = "[G]it [B]rowse" })
     end,
 }
