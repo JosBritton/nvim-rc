9dc27ed64bebdb1f5dc304fbd448bc1345f61820
Joseph Britton <85815005+JoeeBritton@users.noreply.github.com>
2023-12-22 23:31:29 -0800

    Move vim-fugitive and vim-rhubarb to the same plugin file

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,6 @@
 return {
     "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
     init = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
         vim.keymap.set("n", "<leader>gb", vim.cmd.GBrowse, { desc = "[G]it [B]rowse" })
--- lua/plugins/rhubarb.lua
+++
@@ -1,5 +0,0 @@
-return {
-    -- github ext for fugitive
-    "tpope/vim-rhubarb",
-    dependencies = { "tpope/vim-fugitive" },
-}
