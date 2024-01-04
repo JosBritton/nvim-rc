4b08d08715c5da2e0b6870bd71d264b8908fc7e8
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-01-03 20:35:40 -0800

    Revert: fugitive keymap

--- lua/josbritton/plugins/fugitive.lua
+++ lua/josbritton/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "tpope/vim-fugitive" },
     lazy = false,
     init = function()
-        vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "[G.]it Status" })
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
         vim.keymap.set("n", "<leader>gb", vim.cmd.GBrowse, { desc = "[G]it [B]rowse" })
     end,
 }
