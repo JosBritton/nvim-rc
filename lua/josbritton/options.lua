e972dcc0d9c150c6bdaa46140e84db0414dc3719
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-01-03 20:24:17 -0800

    Changes to options.lua - Move to vim.opt rather than vim.o where possible, to avoid using vim   shorthands with lua - Create autocmd to edit formatoption on all buffers

--- lua/josbritton/options.lua
+++ lua/josbritton/options.lua
@@ -1,66 +1,73 @@
 local state = vim.fn.stdpath("state")
 
--- must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- hybrid line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
+-- modify formatoptions for all buffers
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        vim.opt.formatoptions:remove("o")
+    end,
+})
 
--- enable mouse mode
--- vim.o.mouse = "a"
+vim.opt.showcmd = false
+
+-- hybrid line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+-- prevent windows from changing
+vim.opt.equalalways = false
 
 -- use OS clipboard
-vim.o.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 -- https://github.com/neovim/neovim/issues/23650
 --vim.g.netrw_banner = 0
 
-vim.o.breakindent = true
+vim.opt.breakindent = true
 
 -- permanently lost if file is modified externally!
-vim.o.undofile = true
-vim.o.undodir = state .. "/undo//" -- def
+vim.opt.undofile = true
+vim.opt.undodir = state .. "/undo//" -- def
 
-vim.o.backup = true
-vim.o.backupdir = state .. "/backup//"
+vim.opt.backup = true
+vim.opt.backupdir = state .. "/backup//"
 
-vim.o.swapfile = true
+vim.opt.swapfile = true
 
 -- case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-vim.o.smartindent = true
-vim.o.expandtab = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
 
 -- default for when sleuth has NO reference
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
 -- render space for column even if it has no content (prevents text from shifting)
-vim.wo.signcolumn = "yes"
+-- vim.opt.signcolumn = "yes"
 
 -- decrease update time
-vim.o.updatetime = 50
-vim.o.timeoutlen = 300
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
 
-vim.o.completeopt = "menuone,noselect"
+vim.opt.hidden = true
 
-vim.o.termguicolors = true
+vim.opt.completeopt = { "menuone", "noselect" }
 
-vim.o.wrap = false
+vim.opt.termguicolors = true
+
+vim.opt.wrap = false
 
 -- vim.o.cursorline = true
-vim.o.backspace = "indent,eol,start"
+vim.opt.backspace = { "indent", "eol", "start" }
 
--- always center the cursor in the buffer
--- vim.o.scrolloff = 999
+vim.opt.scrolloff = 8
 
-vim.o.scrolloff = 8
-
-vim.o.hlsearch = true
-vim.o.incsearch = true
-
--- vim.o.colorcolumn = "100"
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
