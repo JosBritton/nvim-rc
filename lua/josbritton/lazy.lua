f77efa075d221b89c139225dd70679a7981857bf
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-05-29 23:21:30 -0700

    Use uv instead of loop in lazy init

--- lua/josbritton/lazy.lua
+++ lua/josbritton/lazy.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system {
         "git",
         "clone",
@@ -9,6 +9,8 @@ if not vim.loop.fs_stat(lazypath) then
         lazypath,
     }
 end
+
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("josbritton.plugins", {
