19ca5e913f6cf54d403d5f5b541650bc614d1e63
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-06-24 11:43:09 -0700

    Fix DirEnter event not firing if opened by command
    The BufEnter DirEnter custom event would not fire if neovim was opened
    via `nvim .` but would open correctly when a ZSH widget was used.
    
    This makes sure that auto commands are properly executed and deleted
    after initialization.

--- lua/josbritton/lazy.lua
+++ lua/josbritton/lazy.lua
@@ -13,12 +13,16 @@ end
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+local id = vim.api.nvim_create_augroup("LazyUser", { clear = false })
 vim.api.nvim_create_autocmd("BufEnter", {
-    group = vim.api.nvim_create_augroup("LazyUser", {}),
+    group = id,
+    nested = true,
     callback = function(args)
         local res = vim.loop.fs_stat(vim.api.nvim_buf_get_name(args.buf))
         if res and res.type == "directory" then
+            vim.api.nvim_del_augroup_by_id(id)
             vim.cmd("do User DirEnter")
+            vim.api.nvim_exec_autocmds(args.event, { buffer = args.buf, data = args.data })
         end
     end
 })
