a82d357f47a8d57c5341a72cd45e3e542eadc441
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2024-10-21 01:31:28 -0700

    Use `opt={}` convention for plugins when possible as recommended

--- lua/josbritton/plugins/colorizer.lua
+++ lua/josbritton/plugins/colorizer.lua
@@ -1,7 +1,5 @@
 return {
     "norcalli/nvim-colorizer.lua",
     event = { "BufReadPre", "BufNewFile", "FileReadPre" },
-    config = function()
-        require("colorizer").setup()
-    end,
+    opts = {},
 }
