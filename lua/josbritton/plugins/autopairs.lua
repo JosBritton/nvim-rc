5ebdb088e8a8fcc0bca1ed39199952d67a74e0c6
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-03-30 06:47:12 -0700

    Configure autopairs

--- lua/josbritton/plugins/autopairs.lua
+++ lua/josbritton/plugins/autopairs.lua
@@ -1,7 +1,16 @@
 return {
     "windwp/nvim-autopairs",
-    lazy = false,
-    priority = 100,
     event = "InsertEnter",
-    config = true,
+    opts = {
+        -- assume these effect performance
+        enable_check_bracket_line = true, -- don't add paris when pair is imbalanced on line
+        ignored_next_char = "[%w%.]", -- ignore alphanumeric and `.` symbol
+
+        -- important for generic rules like opening curly brackets
+        map_cr = true,
+
+        -- try to delete pairs
+        map_c_w = true, -- map <C-w> to delete a pair if possible
+        map_c_h = false, -- conflicts with ctrl-backspace map!!
+    },
 }
