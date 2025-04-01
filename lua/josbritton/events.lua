191497dec00fed07a868ad47ee36ca0e2ab46276
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-03-31 23:36:19 -0700

    Refactor DirEnter event

--- lua/josbritton/events.lua
+++ lua/josbritton/events.lua
@@ -1,16 +1,13 @@
 local id = vim.api.nvim_create_augroup("LazyUser", { clear = false })
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
     group = id,
     nested = true,
-    callback = function(args)
-        local res = (vim.uv or vim.loop).fs_stat(vim.api.nvim_buf_get_name(args.buf))
+    callback = function(ev)
+        local res = (vim.uv or vim.loop).fs_stat(vim.api.nvim_buf_get_name(ev.buf))
         if res and res.type == "directory" then
             vim.api.nvim_del_augroup_by_id(id)
             vim.cmd("do User DirEnter")
-            vim.api.nvim_exec_autocmds(
-                args.event,
-                { buffer = args.buf, data = args.data }
-            )
+            vim.api.nvim_exec_autocmds(ev.event, { buffer = ev.buf, data = ev.data })
         end
     end,
 })
