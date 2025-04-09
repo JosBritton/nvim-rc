3ea4ba29015d7ebc89d39f899dc8b437a665ff41
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-04-08 21:14:09 -0700

    Refactor lir lazy loading as a plugin init auto command

--- lua/josbritton/events.lua
+++ lua/josbritton/events.lua
@@ -69,20 +69,6 @@ vim.api.nvim_create_autocmd({ "BufReadPre" }, {
     end,
 })
 
-local id = vim.api.nvim_create_augroup("LazyUser", { clear = false })
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    group = id,
-    nested = true,
-    callback = function(ev)
-        local res = (vim.uv or vim.loop).fs_stat(vim.api.nvim_buf_get_name(ev.buf))
-        if res and res.type == "directory" then
-            vim.api.nvim_del_augroup_by_id(id)
-            vim.cmd("do User DirEnter")
-            vim.api.nvim_exec_autocmds(ev.event, { buffer = ev.buf, data = ev.data })
-        end
-    end,
-})
-
 -- show file immediately before loading plugins
 local id = vim.api.nvim_create_augroup("QuickFile", { clear = true })
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
--- lua/josbritton/plugins/lir.lua
+++ lua/josbritton/plugins/lir.lua
@@ -1,11 +1,11 @@
-return {
+local conf = {
     "tamago324/lir-git-status.nvim",
     dependencies = {
         "JosBritton/lir.nvim",
         "nvim-tree/nvim-web-devicons",
         "nvim-lua/plenary.nvim",
     },
-    event = "User DirEnter",
+    lazy = true,
     keys = {
         { "<leader>pv", nil, desc = "O[p]en File Explorer [V]iew" },
     },
@@ -62,3 +62,29 @@ return {
         })
     end,
 }
+
+conf["init"] = function()
+    local group_id = vim.api.nvim_create_augroup("LoadFileExplorer", { clear = false })
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        group = group_id,
+        once = false, -- must run until/unless a directory is viewed, will remove itself after
+        callback = function(ev)
+            local res = (vim.uv or vim.loop).fs_stat(vim.api.nvim_buf_get_name(ev.buf))
+            if res and res.type == "directory" then
+                local lazy = require("lazy")
+
+                local plugin_name = conf.name
+                local slash = conf[1]:find("/", 1, true)
+                if slash then
+                    plugin_name = conf.name or conf[1]:sub(slash + 1)
+                end
+                lazy.load({ plugins = plugin_name })
+
+                vim.api.nvim_del_autocmd(ev.id)
+                vim.api.nvim_del_augroup_by_id(ev.group)
+            end
+        end,
+    })
+end
+
+return conf
