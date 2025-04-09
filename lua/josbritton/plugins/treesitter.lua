992c6405a74b964b79ee78a3d9d92505fa16e5a6
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-04-08 22:48:07 -0700

    Remove treesitter setup logic regarding deferred loading

--- lua/josbritton/plugins/treesitter.lua
+++ lua/josbritton/plugins/treesitter.lua
@@ -40,90 +40,86 @@ return {
         "TSTextobjectPeekDefinitionCode",
         "TSTextobjectSelect",
     },
-    -- load treesitter only after first render of a buffer has completed
-    -- (defer setup for 0ms)
-    config = function()
-        vim.defer_fn(function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "c",
-                    "cpp",
-                    "go",
-                    "lua",
-                    "python",
-                    "rust",
-                    "tsx",
-                    "javascript",
-                    "typescript",
-                    "vimdoc",
-                    "vim",
-                    "bash",
-                    "markdown",
+    main = "nvim-treesitter.configs",
+    config = true,
+    opts = {
+        ensure_installed = {
+            "c",
+            "cpp",
+            "go",
+            "lua",
+            "python",
+            "rust",
+            "tsx",
+            "javascript",
+            "typescript",
+            "vimdoc",
+            "vim",
+            "bash",
+            "markdown",
+        },
+        auto_install = true,
+        sync_install = false,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = { "ruby" },
+        },
+        indent = {
+            enable = true,
+            disable = { "ruby" },
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                -- init_selection = "<c-space>",
+                -- node_incremental = "<c-space>",
+                scope_incremental = "<c-s>",
+                node_decremental = "<M-space>",
+            },
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- you can use the capture groups defined in textobjects.scm
+                    ["aa"] = "@parameter.outer",
+                    ["ia"] = "@parameter.inner",
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
                 },
-                auto_install = true,
-                sync_install = false,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = { "ruby" },
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
                 },
-                indent = {
-                    enable = true,
-                    disable = { "ruby" },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
                 },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        -- init_selection = "<c-space>",
-                        -- node_incremental = "<c-space>",
-                        scope_incremental = "<c-s>",
-                        node_decremental = "<M-space>",
-                    },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
                 },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
-                        keymaps = {
-                            -- you can use the capture groups defined in textobjects.scm
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["<leader>a"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["<leader>A"] = "@parameter.inner",
-                        },
-                    },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
                 },
-            })
-        end, 0)
-    end,
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
+            },
+        },
+    },
 }
