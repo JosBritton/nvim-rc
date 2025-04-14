5ba87d24b5b2bb0a5ee875995d1eded21927908f
Joseph Britton <85815005+JosBritton@users.noreply.github.com>
2025-04-14 02:26:15 -0700

    Only set markdown settings for normal buffers

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,5 @@
-vim.opt_local.spell = true
-vim.opt_local.wrap = true
-vim.opt_local.colorcolumn = "80" -- limit is 80 on markdownlint
+if vim.bo.buftype == "" then -- only normal markdown buffers
+    vim.opt_local.spell = true
+    vim.opt_local.wrap = true
+    vim.opt_local.colorcolumn = "80" -- limit is 80 on markdownlint
+end
